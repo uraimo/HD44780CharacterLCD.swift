12dd391731bc5c31987294bd2ff35ded38a09add
Umberto Raimondi <me@uraimo.com>
2016-02-22 18:16:56 +0100

    Added dependency from SwiftyGPIO

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "HD44780LCD"
+    name: "HD44780LCD",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git",
+                 majorVersion: 1)
+    ]
 )
