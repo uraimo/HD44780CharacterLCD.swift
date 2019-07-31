74163bd9fe07e225a92ecd400a8de0f537ccbdaf
Umberto Raimondi <me@uraimo.com>
2019-07-31 09:52:40 +0200

    Backported changes to 4.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
@@ -14,7 +14,8 @@ let package = Package(
             name: "HD44780LCD",
             dependencies: [
                 "SwiftyGPIO"
-            ]
+            ],
+            path: "Sources"
         )
     ]
 )
