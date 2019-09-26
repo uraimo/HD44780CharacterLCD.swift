f946f9bb5a573c267bb4fbd6a45557f717eec8ca
Umberto Raimondi <me@uraimo.com>
2019-09-26 18:35:46 +0200

    Fixed example

--- Examples/RaspberryPi/Package.swift
+++ Examples/RaspberryPi/Package.swift
@@ -1,8 +1,15 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "TestLCD",
     dependencies: [
-        .package(url: "https://github.com/uraimo/HD44780CharacterLCD.swift.git", from: "3.0.0"),
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0"),
+        .package(url: "https://github.com/uraimo/HD44780CharacterLCD.swift.git",from: "3.0.0")
+    ],
+    targets: [
+        .target(name: "TestLCD", 
+                dependencies: ["SwiftyGPIO","HD44780LCD"],
+                path: "Sources")
     ]
-)
+) 
